c9f704c1613f3e671c58f0a54aa0a7b75c597683
Gordon <gordonguan13@gmail.com>
2026-08-31 16:16:55 -0700

    day 2

---
+++ biggest.py
@@ -0,0 +1,11 @@
+num1 = int(input("number 1: "))
+num2 = int(input("number 2: "))
+num3 = int(input("number 3: "))
+biggest = num1
+if num2>biggest:
+    biggest=num2
+if num3>biggest:
+    biggest=num3
+print(biggest)
+
+
