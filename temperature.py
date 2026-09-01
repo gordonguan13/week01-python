e8101d953ee27b6968d66f52847c45a7e31c7173
Gordon <gordonguan13@gmail.com>
2026-08-31 19:37:03 -0700

    day 3

--- temperature.py
+++ temperature.py
@@ -1,3 +1,6 @@
-f = int(input("put in farenheit: "))
-c= round((f-32) * 5/9,2)
-print("celcius: ", c)
+def faren(f):
+    return (f-32) * 5/9
+def celc(c):
+    return c * 9/5 + 32
+print(faren(100))
+print(celc(32))
