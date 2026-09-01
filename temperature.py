6cdae9ecd9ae9ecd4ad8ba68e5d604f1cb604025
Gordon <gordonguan13@gmail.com>
2026-08-31 19:45:44 -0700

    day 3

--- temperature.py
+++ temperature.py
@@ -1,6 +1,7 @@
-def faren(f):
-    return (f-32) * 5/9
-def celc(c):
-    return c * 9/5 + 32
+def faren(c):
+    return (c-32) * 5/9
+def celc(f):
+    return f * 9/5 + 32
 print(faren(100))
 print(celc(32))
+print(faren(celc(50)))
