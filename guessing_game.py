e5f5417db2ccacd10e881850e550f9c5619cd2b2
Gordon <gordonguan13@gmail.com>
2026-09-03 20:05:15 -0700

    day 5, project

--- guessing_game.py
+++ guessing_game.py
@@ -1,30 +1,56 @@
 import random
-num=random.randint(1,100)
-finished = False
-tries = 0
+best = None
+def getdifficulty():
+    choice=''
+    while choice not in ["easy", "medium", "hard"]:
+        choice = input("difficulty (easy,medium,hard)? ")
+    if choice == 'easy':
+        return 50
+    elif choice == 'medium':
+        return 100
+    else:
+        return 1000  
 def calculate(guess, num):
-    if int(guess) != num:
-        if int(guess) > num:
-            return "high"
-        else:
-            return "low"
+    if guess > num:
+        return "high"
+    else:
+        return "low"
 def getguess():
-    a = True
-    while a:
+    while True:
         try:
-            return int(input("What number?"))
-            a = False
+            return int(input("What number? "))
         except ValueError:
             print("thats not a number.")
-print(num)
-
-while finished == False:
-    g=getguess()
-    if g == num:
-        tries += 1
-        print("You got it!") 
-        finished = True
-        print("Tries:", tries)
+def tryagain():
+    ttt = ''
+    while ttt not in ["y","n"]:
+        ttt = input("Would you like to play again? (y or n)")
+    if ttt == "n":
+        return True
     else:
-        print("Incorrect number. Too", calculate(g, num))
-        tries += 1 
+        return False
+
+        
+
+finish = False
+while finish == False:
+    difficulty=getdifficulty()
+    num=random.randint(1,difficulty)
+    complete = False
+    tries = 0
+    print(num)
+    while complete == False:
+        g=getguess()
+        if g == num:
+            tries += 1
+            print("You got it!") 
+            complete = True
+            print("Tries:", tries)
+            if best is None or tries < best:
+                best = tries
+            print("Best tries so far:", best)
+            finish = tryagain()
+        else:
+            print("Incorrect number. Too", calculate(g, num))
+            tries += 1
+    # best tries
